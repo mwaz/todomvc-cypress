35de0dd6d139aa59af9b3ceb63a2ec0b6f7cf4a2
mwaz <waweru.mwaura@allangray.co.za>
2020-04-20 10:26:39 +0300

    fix: cypress test files

--- cypress/pages/todomvc-page-objects.js
+++ cypress/pages/todomvc-page-objects.js
@@ -1,18 +1,27 @@
+
 const elements = {
     newTodo: () => cy.get('.new-todo'),
     clickCompleted : () => cy.get('.toggle'),
     clearCompleted : () => cy.get('.clear-completed'),
     todoLabel : () => cy.get('label'),
+    todoList: () => cy.get('.todo-list')
 }
 
 const actions = {
     addTodo: (todoName) => elements.newTodo().type(`${todoName}`),
-    markCompleted: () => elements.clickCompleted().click(),
-    clearCompleted: () => elements.clearCompleted().click(),
+    markCompleted: () => {
+        elements.clickCompleted().click();
+        elements.todoLabel().should('have.css', 'text-decoration-line', 'line-through');
+    },
+    clearCompleted: () =>  {
+        elements.clearCompleted().click();
+        elements.todoList().should('not.have.descendants', 'li');
+    },
 }
 
 const expects = {
-    checkTodoExists: (todoName) => elements.todoLabel().should('have.text', `${todoName}`)
+    checkTodoExists: (todoName) => elements.todoLabel().should('have.text', `${todoName}`),
+    checkUncompletedTodo: () => elements.clickCompleted().should('not.be.checked')
 
 }
 
--- cypress/tests/todomvc.spec.js
+++ cypress/tests/todomvc.spec.js
@@ -2,12 +2,26 @@
 import TodoPageObjects from '../pages/todomvc-page-objects';
 
 describe('Todo MVC tests', () => {
-    beforeEach( () => cy.visit('http://todomvc-app-for-testing.surge.sh/') )
-    it('should be able to addd a new todo to the list', () => {
-       
+    
+    beforeEach( () => {
+        cy.visit('http://todomvc-app-for-testing.surge.sh/')
         TodoPageObjects.actions.addTodo('cleanhouse{enter}');
-        TodoPageObjects.expects.checkTodoExists('cleanhouse');
-        TodoPageObjects.actions.markCompleted();
-
     })
+    it('should be able to add a new todo to the list', () => {
+       
+        TodoPageObjects.expects.checkTodoExists('cleanhouse');
+        TodoPageObjects.expects.checkUncompletedTodo();
+        TodoPageObjects.actions.markCompleted();
+    });
+
+    it('should mark a todo as completed', () => {
+        TodoPageObjects.expects.checkUncompletedTodo();
+        TodoPageObjects.actions.markCompleted();
+    });
+
+    it('should clear completed Todos', () => {
+        TodoPageObjects.actions.markCompleted();
+        TodoPageObjects.actions.clearCompleted();
+    })
+
 })
